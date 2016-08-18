992be0f3a9939dac595949d3e8851a6bcf72c6c9
wangwei <wang.wei@edobee.com>
2016-08-18 18:05:44 +0800

    修改首页

--- perfect-web/resource/js/index.js
+++ perfect-web/resource/js/index.js
@@ -1,8 +1,14 @@
 var $hideLi = $('.perfect-nav .hide-li');
 var $perfectPills = $('.perfect-pills');
-$hideLi.on('mouseover',function(e){
-  $perfectPills.show();
-});
-$hideLi.on('mouseout',function(e){
-  $perfectPills.hide();
+
+var tag = true;
+$hideLi.on('click', function (e) {
+  if (tag) {
+    $perfectPills.show();
+    tag = false;
+  }
+  else {
+    $perfectPills.hide();
+    tag = true;
+  }
 });
