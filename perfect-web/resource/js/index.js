636693047228dc556321aefb147b1f6ad27191a7
wangwei <wang.wei@edobee.com>
2016-08-30 17:13:47 +0800

    代码优化

--- perfect-web/resource/js/index.js
+++ perfect-web/resource/js/index.js
@@ -1,19 +1,7 @@
 import {formatDate} from './common/format-time';
 import {myApp} from './main';
 
-//动态区域
-function setDynamicHTML(opt) {
-  return '<div class="title-content">' +
-    '<img class="title-img" src="public/images/a0.jpg">' +
-    '<p class="title-name">' + opt.name + '</p>' +
-    '<p class="title-time">' + opt.time + '</p>' +
-    '<p class="title-text">' + opt.text + '</p>' +
-    '</div>'
-
-}
-
 //首页
-
 myApp.controller('indexCtrl', function ($scope, server) {
   var $hideLi = $('.perfect-nav .hide-li');
   var $perfectPills = $('.perfect-pills');
@@ -30,13 +18,9 @@ myApp.controller('indexCtrl', function ($scope, server) {
   });
   server.post('/getDynamic', {}, function (err, result) {
     if (!err) {
-      $.each(result.result, function (i, r) {
-        var opts = {
-          name: r.username,
-          time: formatDate(r.creationDate),
-          text: r.dynamic_text
-        };
-        $('.dynamic-eare').append(setDynamicHTML(opts));
+      $scope.results = result.result;
+      $.each($scope.results, function (i, r) {
+        r.creationDate = formatDate(r.creationDate);
       })
     }
   })
@@ -52,17 +36,12 @@ myApp.controller('dynamicCtrl', function ($scope, server) {
       'date': new Date().getTime()
     }, function (err, data) {
       if (!err) {
-        $('.dynamic-eare').empty();
         server.post('/getDynamic', {}, function (err, result) {
           if (!err) {
             toastr.success('发表成功');
-            $.each(result.result, function (i, r) {
-              var opts = {
-                name: r.username,
-                time: formatDate(r.creationDate),
-                text: r.dynamic_text
-              };
-              $('.dynamic-eare').append(setDynamicHTML(opts));
+            $scope.results = result.result;
+            $.each($scope.results, function (i, r) {
+              r.creationDate = formatDate(r.creationDate);
             })
           }
         })
