957f50ce92876aab65dc3f740c1c52322dd53a8d
wangwei <wang.wei@edobee.com>
2016-09-08 09:47:21 +0800

    代码优化

--- perfect-web/resource/js/login.js
+++ perfect-web/resource/js/login.js
@@ -1,23 +1,4 @@
-//import {Service} from './common/service'
-//import {mainApp} from './common/angular-jq'
-//var $loginContainer = $('.container-login');
-//var $username = $('.username',$loginContainer);
-//var $password = $('.password',$loginContainer);
-//var service = new Service();
-//
-//$loginContainer.on('click','.login-btn',function(e){
-//  e.preventDefault();
-//  service.ajaxPost('/ajaxLogin',{username:$username.val(),password:$password.val()},function(err,result){
-//    if(!err){
-//      window.location.href = '/';
-//    }
-//    else{
-//
-//      $('.err-msg').css('visibility','visible');
-//      $('.err-msg').text(err.msg);
-//    }
-//  })
-//});
+
 var myApp = angular.module("myApp", []);
 myApp.controller('loginCtrl', function($scope,$http) {
   $scope.firstName = '';
